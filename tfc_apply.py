73df5dcfe46a0d1d2700756d831157cc1b1ef86a
nc7 <nc7@illinois.edu>
2020-09-18 22:33:13 -0500

    tfc work

--- tfc_apply.py
+++ tfc_apply.py
@@ -1,3 +1,4 @@
 from tfci import compress
 
-compress('hific-lo', "./images/LR/0801x4.png", None)
+compress('hific-lo', "./images/LR/0801x4.png", "./images/tfc_comp/0801x4.tfci")
+decompress("./images/tfc_comp/0801x4.tfci", "./images/tfc_decomp/0801x4.png")
