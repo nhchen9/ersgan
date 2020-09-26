4f214cb556ce7d632f85dfd293887fda339096d5
nhchen9 <nc7@illinois.edu>
2020-09-26 05:04:22 +0000

    another model

--- tfc_apply.py
+++ tfc_apply.py
@@ -4,5 +4,5 @@ import glob
 test_img_folder = 'images/4kDS/*'
 
 for path in glob.glob(test_img_folder):
-    compress("bmshj2018-hyperprior-msssim-8", path, path.replace("4kDS", "tfc_comp_bm").replace("png","tfci"))
-    decompress(path.replace("4kDS","tfc_comp_bm").replace("png","tfci"), path.replace("4kDS","tfc_decomp_bm"))
+    compress("mbt2018-mean-msssim-8", path, path.replace("4kDS", "tfc_comp_mbt").replace("png","tfci"))
+    decompress(path.replace("4kDS","tfc_comp_mbt").replace("png","tfci"), path.replace("4kDS","tfc_decomp_mbt"))
