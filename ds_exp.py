a112232f8c8bed235ad037d9f638ba5cdd3f7b7d
nc7 <nc7@illinois.edu>
2020-09-25 19:17:56 -0500

    updating test scripts

--- ds_exp.py
+++ ds_exp.py
@@ -10,7 +10,7 @@ model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
-test_img_folder = 'images/HR/*'
+test_img_folder = 'images/4k/*'
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
@@ -31,11 +31,11 @@ for path in glob.glob(test_img_folder):
     base_img = img.copy()
     ds_factor = 4
     img = cv2.resize(img, (int(img.shape[1]/ds_factor), int(img.shape[0]/ds_factor)), interpolation = cv2.INTER_AREA)
-    cv2.imwrite('images/DS/{:s}_rlt.png'.format(base), img)
+    cv2.imwrite('images/4kDS/{:s}_rlt.png'.format(base), img)
 
-    LR_cp = cv2.imread('images/LR/{:s}x4.png'.format(base.split('_')[0]), cv2.IMREAD_COLOR)
-    print(ssim(LR_cp, img, multichannel = True))
-    ds_lr_ssim.append(ssim(LR_cp, img, multichannel = True))
+    #LR_cp = cv2.imread('images/LR/{:s}x4.png'.format(base.split('_')[0]), cv2.IMREAD_COLOR)
+    #print(ssim(LR_cp, img, multichannel = True))
+    #ds_lr_ssim.append(ssim(LR_cp, img, multichannel = True))
 
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
@@ -44,13 +44,13 @@ for path in glob.glob(test_img_folder):
     img_LR = img_LR.to(device)
 
     with torch.no_grad():
-        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = model(img_LR).cpu().data.squeeze().float().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite('images/results/{:s}_rlt.png'.format(base), output)
+    cv2.imwrite('images/results_4k/{:s}.png'.format(base), output)
     print(ssim(base_img, output, multichannel = True))
     out_hr_ssim.append(ssim(base_img, output, multichannel = True))
 
 print("finished.  avg stats:")
-print("ds-lr ssim:", np.mean(ds_lr_ssim))
+#print("ds-lr ssim:", np.mean(ds_lr_ssim))
 print("out-hr ssim:", np.mean(out_hr_ssim))
